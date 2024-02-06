6abb4ef6cd42865fd5f264ed1e75b5d55f45830c
krishna300 <srikrishnakvs472@gmail.com>
2024-02-06 18:15:05 +0530

    Dashboard navbar

--- src/screens/DashBoard.jsx
+++ src/screens/DashBoard.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React, {useState} from 'react'
+import { Outlet, Link} from 'react-router-dom';
+import { RiCloseLine } from "react-icons/ri";
+import { MdMenu } from "react-icons/md";
+
+
 const DashBoard = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+
+  const handleOpen = () => {
+    setIsOpen(true)
+  }
+  const menuContainer = {
+    display:'flex',
+    flexDirection:'column'
+  }
+  const leftNavBar = {
+    // visibility:isOpen?'visible':'hidden',
+    height:'100vh',
+    backgroundColor:'#786867',
+    width:'200px',
+  }
+  const menuItem = {
+    textDecoration:'none',
+    fontSize:'21px',
+    padding:'12px',
+  }
+
+  const outletContainer = {
+    padding:'20px',
+    margin:'auto',
+    // border:'2px solid red',
+    width:'80vw',
+    height:'94vh',
+  }
+
   return (
-    <div>
-      
-      <h2>DashBoard</h2>
-      <Outlet/>
+    <div style={{
+      display:'flex'
+    }}>
+      {!isOpen && <MdMenu 
+        size={24} 
+        style={{padding:'15px', color:'white'}}
+        onClick={handleOpen}
+      />}
+      {isOpen && <div style={leftNavBar}>
+        <div style={{display:'flex'}}>
+          <h2 style={{padding:'12px'}}>DashBoard</h2>
+          <RiCloseLine 
+            size={23} 
+            style={{padding:'12px'}}
+            onClick={handleClose}
+          />
+        </div>
+        <div style={menuContainer}>
+          <Link style={menuItem} to='/firstModal'>First Modal</Link>
+          <Link style={menuItem} to='/secondModal'>Second Modal</Link>
+        </div>
+
+      </div>}
+      <div style={outletContainer}>
+        <Outlet/>
+      </div>
     </div>
   )
 }
--- src/screens/FirstModal.jsx
+++ src/screens/FirstModal.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+//
+import post_mb from '../assets/post_mb.jpg';
+import post_pk from '../assets/post_pk.jpg';
+import post_prabhas from '../assets/post_prabhas.jpg';
+import post_aa from '../assets/post_aa.jpg';
+import post_rc from '../assets/post_rc.jpg';
 
 const FirstModal = () => {
+
+  const images = [
+    {
+      id:1,
+      src:post_aa
+    },
+    {
+      id:2,
+      src:post_pk
+    },
+    {
+      id:3,
+      src:post_prabhas
+    },
+    {
+      id:4,
+      src:post_mb
+    },
+    {
+      id:5,
+      src:post_rc
+    }
+  ]
   return (
-    <div>FirstModal</div>
+    <div>
+      <h2 style={{textTransform: 'capitalize'}}>Gundata</h2>
+      {images.map((actor) =>{
+        return(<>
+          <img 
+            width="200px" height="200px"
+            src={actor.src}
+            style={{borderRadius:'12px'}}  
+          />
+        </>)
+      })}
+    </div>
   )
 }
 
